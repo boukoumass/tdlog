1aaca732b766c67c1ed07def892eabd7dee79952
Boukoumass Khadija <99352025+boukoumass@users.noreply.github.com>
2022-11-02 21:12:42 +0100

    Add files via upload

--- TDlog.py
+++ TDlog.py
@@ -1,15 +1,24 @@
-def factoriel(n):
-    if n==0:
-        return 1
-    else:
-        return n*factoriel(n-1)
-
-print(factoriel(5))
-
-from unittest import TestCase
-from factoriel import factoriel
-
-class Test(TestCase):
-    def test_factoriel(self):
-        self.assertEqual(1, factoriel(0))
-        self.assertEqual(24, factoriel(4))
+#les armes:
+class weapon:
+    def __init__(self, ammunitions, range):
+        self.ammunitions = ammunitions
+        self.range = range
+    def fire_at(self, x:float, y:float, z:float):
+        print(f"fire at {x,y,z}")
+    def getrange(self):
+        print(self.range)
+Lance_missiles_antisurface=weapon(40,30)
+Lance-missiles antiair=weapon(50,40)
+Lance-torpilles=weapon(15,20)
+Lance_missiles_antisurface.fire_at(2,5,6)
+class Vessel:
+    def __init__(self, coordinates, weapon, maxhits):
+        self.coordinates = coordinates
+        self.weapon = weapon
+        self.maxhits = maxhits
+    def getcoordinates(self):
+        return self.coordinates
+    def go_to(self,x,y,z):
+        return (x,y,z)
+    def fire_at(self,x,y,z):
+        return (x,y,z)
